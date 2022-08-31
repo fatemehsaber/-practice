2ebafb8ad21020f5a0800a9bcbb0608dee9e47bf
fatemehsaber <110448233+fatemehsaber@users.noreply.github.com>
2022-08-31 18:09:21 +0430

    Update TEXT_ADRESS_Qr.py

--- practice4/TEXT_ADRESS_Qr.py
+++ practice4/TEXT_ADRESS_Qr.py
@@ -1,7 +1,10 @@
 import qrcode
 
-text_adress = input('enter the text and adress : ')
+text = input("Enter the text: ")
+address = input("Enter the address: ")
 
+qr1 = qrcode.make(text)
+qr2 = qrcode.make(text)
 
-Qr = qrcode.make(text_adress)
-Qr.save('test_adress.jpg')
+qr1.save("qrcode1.jpg")
+qr2.save("qrcode2.jpg")
